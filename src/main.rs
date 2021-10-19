e70b56d5e6ee7b18c35a532a6272f3b6fc986f2e
Thvarzil <louisblebohec@gmail.com>
2021-10-19 15:41:30 -0600

    now accepting command line arguments and reading a file with name entered by user

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    //collects launch arguments, stores relevant ones in variables.
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {} in file {}", query, filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
